376dda7fa302a7ab776cd7e478d8c338cde15b04
Andres Rivillas <andresrivillas@Andress-MacBook-Pro.local>
2025-11-15 20:30:51 -0500

    Analyze sentiment in text

---
+++ HikingSurvey/HikingSurvey/HikingSurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingSurveyApp.swift
+//  HikingSurvey
+//
+//  Created by Andres Rivillas on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct HikingSurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
